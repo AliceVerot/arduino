46375e55f8689248b15a26c53b571a953720195a
MilanMM <milanmilosevic2001@yahoo.fr>
2018-04-11 15:31:27 +0200

    Je ne connais pas la termnologoie de void

--- main.ino
+++ main.ino
@@ -6,9 +6,14 @@
 // the setup function runs once when you press reset or power the board
 void setup()
 {
+    pinMode (6, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
+    digitalWrite (6, HIGH);
+    delay (1000);
+    digitalWrite(6, LOW);
+    delay(1000);
 }
